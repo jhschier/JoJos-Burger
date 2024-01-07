a72f4239d69ef096e4771a9c751f4296681fad8f
jhschier <jhschier30@hotmail.com>
2024-01-07 00:25:36 +0000

    add: configs database.js

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'jhschier',
+    database: 'jojosburger',
+    define: {
+        timestamps: true,
+        underscore: true,
+        underscoreAll: true
+    }
+}
