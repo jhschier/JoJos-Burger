c2b81efab5da868eb6aba2b37bff544ccb122d6b
jhschier <jhschier30@hotmail.com>
2024-01-08 20:48:49 +0000

    Add: adding configuration of model User and connection to database

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,19 +1,19 @@
-import Sequelize, { Model } from 'sequelize'
-
+import Sequelize, { Model } from "sequelize";
 
 class User extends Model {
-    static init(sequelize)
-
-    super.init({
+  static init(sequelize) {
+    super.init(
+      {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
         password_hash: Sequelize.STRING,
         admin: Sequelize.BOOLEAN,
-    },
-    {
+      },
+      {
         sequelize,
-    }
-    )
+      }
+    );
+  }
 }
 
-export default User
+export default User;
