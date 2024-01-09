e35dca7f4bca7971f6594c9ced667e53569dafc7
jhschier <jhschier30@hotmail.com>
2024-01-09 19:54:20 +0000

    Add: Email verification, in case the email is already being used, shows a message indicating it.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -27,6 +27,14 @@ class UserController {
 
     const { name, email, password_hash, admin } = request.body;
 
+    const userExists = await User.findOne({
+      where: { email },
+    });
+
+    if (userExists) {
+      return response.status(400).json({ error: "Email already in use." });
+    }
+
     const user = await User.create({
       id: v4(),
       name,
