0055f0e3a3ad3765a8bc53055333c403b4158b57
jhschier <jhschier30@hotmail.com>
2024-01-09 17:24:43 +0000

    Add/Change: First Controller for User and test with postman succeeded, info stored in db's table. Change: added UserController to routes.

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,9 @@
 import { Router } from "express";
-import { v4 } from "uuid";
 
-import User from "./app/models/User";
+import UserController from "./app/controllers/UserController";
 
 const routes = new Router();
 
-routes.get("/", async (request, response) => {
-  const users = await User.create({
-    id: v4(),
-    name: "JoJo",
-    email: "jhschier30@hotmail.com",
-    password_hash: "123321",
-    admin: true,
-  });
-
-  return response.json(users);
-});
+routes.post("/users", UserController.store);
 
 export default routes;
