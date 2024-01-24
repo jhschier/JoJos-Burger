9029fa0553207bc9aac8f82a718206b38ff0b933
jhschier <jhschier30@hotmail.com>
2024-01-24 16:44:30 +0000

    Add: Creating relation between Category and Product. (category_id from Category being inserted in Product)

--- src/database/index.js
+++ src/database/index.js
@@ -15,7 +15,11 @@ class Database {
 
   init() {
     this.connection = new Sequelize(configDatabase);
-    models.map((model) => model.init(this.connection));
+    models
+      .map((model) => model.init(this.connection))
+      .map(
+        (model) => model.associate && model.associate(this.connection.models)
+      );
   }
 }
 
