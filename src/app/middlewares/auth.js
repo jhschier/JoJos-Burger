dc0914f692b937f6a6ba7d9e8323f426dd020abe
jhschier <jhschier30@hotmail.com>
2024-03-14 21:52:57 +0000

    Test: Searching for error in auth.js

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -4,15 +4,20 @@ import authConfig from "../../config/auth.js";
 export default (request, response, next) => {
   const authToken = request.headers.authorization;
 
+  console.log('Received token:', authToken);
+
   if (!authToken) {
     return response.status(401).json({ error: "Token not provided." });
   }
 
   const token = authToken.split(" ")[1];
 
+  console.log('Extracted token:', token);
+
   try {
     jwt.verify(token, authConfig.secret, function (err, decoded) {
       if (err) {
+        console.error('Error verifying token:', err);
         throw new Error();
       }
 
@@ -21,6 +26,7 @@ export default (request, response, next) => {
       return next();
     });
   } catch {
+    console.error('Error verifying token');
     return response.status(401).json({ error: "Token is invalid" });
   }
 };
